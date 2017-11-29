428e993496fc56d6bfbc579a2f7a1e601e22300e
Andrew Wagner <ahwagner@outlook.com>
2017-11-29 10:18:20 -0500

    ignore .gitmodules in symlink.sh

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for DOT in `find $(git rev-parse --show-toplevel)  -name ".*" -not -name "." -not -name ".git" -not -name "*.swp" -maxdepth 1`
+for DOT in `find $(git rev-parse --show-toplevel)  -name ".*" -not -name "." -not -name ".git" -not -name ".gitmodules" -not -name "*.swp" -maxdepth 1`
 do
 	ln -fsv "$DOT" ~
 done
