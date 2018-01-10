33987a1d1ed83b8a9cf92402e86d47bc9d343b70
Andrew Wagner <ahwagner@outlook.com>
2018-01-10 11:43:28 -0500

    add haskell-platform to pack-opt-install.sh

--- scripts/darwin/pack-opt-install.sh
+++ scripts/darwin/pack-opt-install.sh
@@ -6,6 +6,7 @@ brew cask install 1password
 brew cask install docker
 brew cask install figma
 brew cask install firefox
+brew cask install haskell-platform
 brew cask install java
 brew cask install racket
 brew cask install silverlight
